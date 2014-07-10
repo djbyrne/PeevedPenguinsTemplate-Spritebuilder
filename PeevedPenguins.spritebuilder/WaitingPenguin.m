e8b9a03903a7ada37d0aec81ab952e36a4a469ee
djbyrne <donaljbyrne@me.com>
2014-07-10 20:11:57 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -10,12 +10,12 @@
 
 @implementation WaitingPenguin
 
--(void)didLoadFromCCB
+- (void)didLoadFromCCB
 {
-    //generate a random number between 0 and 2
-    float delay = (arc4random()%2000)/1000.f;
-    //call method to start animation
-    [self performSelector:@selector(startBlinkAndJump)withObject:nil afterDelay:delay];
+    // generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    // call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
 - (void)startBlinkAndJump
